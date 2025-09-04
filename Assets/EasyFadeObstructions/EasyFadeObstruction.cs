15f79ee42a5db7012416058c9728049a9a66c954
Prashant Gupta <57870705+prashant1998gupta@users.noreply.github.com>
2025-09-04 12:52:11 +0530

    added

--- Assets/EasyFadeObstructions/EasyFadeObstruction.cs
+++ Assets/EasyFadeObstructions/EasyFadeObstruction.cs
@@ -1,165 +1,64 @@
 ﻿using UnityEngine;
-using System.Collections;
 using System.Collections.Generic;
 
-public class EasyFadeObstruction : MonoBehaviour {
-	public GameObject playerObject;
-	public float fadeRadius = 0.5f;
-	public float fadeDistanceOffset = 0;
-	public LayerMask fadeMaskLayer;
-	public float fadeInSpeed = 10f;
-	public float fadeOutSpeed = 10f;
-	[Range(0.0f, 1.0f)]
-	public float fadeTransparency;
+public class EasyFadeObstruction : MonoBehaviour
+{
+    public Transform target;
+    public LayerMask fadeMask;
+    public float fadeRadius = 0.3f;
+    public float fadeOffset = 0f;
+    public float fadeOutSpeed = 6f;
+    public float fadeInSpeed = 6f;
+    [Range(0f, 1f)]
+    public float obstructAlpha = 0.2f;
 
-	HashSet<GameObject> fadeSet;
-	List<GameObject> toRemove;
-	HashSet<Renderer> fadingBack;
-	List<Renderer> removeFadingBack;
+    HashSet<Renderer> obstructed = new HashSet<Renderer>();
+    HashSet<Renderer> stillObstructed = new HashSet<Renderer>();
 
-	void Start () {
-		fadeSet = new HashSet<GameObject>();
-		toRemove = new List<GameObject>();
-		fadingBack = new HashSet<Renderer>();
-		removeFadingBack = new List<Renderer>();
+    void LateUpdate()
+    {
+        if (!target) return;
 
-	}
+        // 1. Cast to target
+        Vector3 dir = target.position - transform.position;
+        float dist = dir.magnitude + fadeOffset;
 
-	void Awake(){
-		fadeSet = new HashSet<GameObject>();
-		toRemove = new List<GameObject>();
-		fadingBack = new HashSet<Renderer>();
-		removeFadingBack = new List<Renderer>();
-	}
-	
-	
-	
-	Color tempColor;
-	void makeTransparent(Renderer r){
-		tempColor = r.material.color;
-		tempColor.a = Mathf.Lerp(tempColor.a, fadeTransparency, Time.deltaTime * fadeOutSpeed);
-		r.material.color = tempColor;
-	}
-	
-	void makeSolid(Renderer r){
+        RaycastHit[] hits = Physics.SphereCastAll(
+            new Ray(transform.position, dir),
+            fadeRadius, dist, fadeMask,
+            QueryTriggerInteraction.Ignore);
 
-		fadingBack.Add(r);
-		/*tempColor = r.material.color;
-		tempColor.a = 1f;
-		r.material.color = tempColor;*/
-	}
+        stillObstructed.Clear();
+        foreach (var h in hits)
+        {
+            var r = h.collider.GetComponent<Renderer>();
+            if (r == null) continue;
 
-	void fadeBackObjects(){
-		Color c;
-		if(fadingBack != null){
-			foreach(Renderer r in fadingBack){	
-				try{
-					if(!fadeSet.Contains(r.gameObject)){
-						c = r.material.color;
-						c.a = Mathf.Lerp(c.a, 1.0f, Time.deltaTime * fadeInSpeed);
-						r.material.color = c;
-						if(1.0 - c.a <= 0.1f){
-							c.a = 1.0f;
-							r.material.color = c;
-							removeFadingBack.Add(r);
-						}
-					}
-				}catch(MissingReferenceException mre){
-					//No need to announce the exception, reference will be cleared from the to be removed list
-					mre.GetType();
-					fadingBack.Remove(r);
-				}
+            stillObstructed.Add(r);
 
-			}
-			foreach(Renderer r in removeFadingBack){
-				fadingBack.Remove(r);
-			}
-			removeFadingBack.Clear();
-		}
-	}
-	
+            if (!obstructed.Contains(r))
+                obstructed.Add(r);
+        }
 
+        // 2. Fade obstructed
+        foreach (var r in obstructed)
+        {
+            if (!r) continue;
+            float targetA = stillObstructed.Contains(r) ? obstructAlpha : 1f;
+            FadeTo(r, targetA, stillObstructed.Contains(r) ? fadeOutSpeed : fadeInSpeed);
+        }
 
-	Vector3 direction;
-	float distance;
-	bool exists = false;
-	void fadeBlockingObjects(){
+        // 3. Remove cleaned-up renderers
+        obstructed.RemoveWhere(r => !r);
+    }
 
-		try{
-			direction = playerObject.transform.position - transform.position;
-		}catch(UnassignedReferenceException e){
-			Debug.LogWarning(e.GetType()+": GameObject not assigned to playerObject variable in EasyFadeObstruction.");
-			this.enabled = false;
-		}
-		distance = direction.magnitude - fadeRadius + fadeDistanceOffset;
-		
-		RaycastHit[] hits = Physics.SphereCastAll( new Ray(transform.position, direction), fadeRadius, distance, fadeMaskLayer);
-			//RaycastAll(transform.position, direction , distance, fadeMaskLayer);
-		try{
-		//if there is nothing being hit, clear fade set
-		if(hits.Length > 0){
-			foreach(RaycastHit h in hits){
-				if(h.collider.gameObject.GetComponent<Renderer>() != null){
-					fadeSet.Add(h.collider.gameObject);
-					//h.collider.gameObject.GetComponent<Renderer>().enabled = false;
-					makeTransparent(h.collider.gameObject.GetComponent<Renderer>());
-				}
-			}
-			if(fadeSet != null){
-				//for every object that exists, add it to the fade set
-				foreach(GameObject g in fadeSet){
-					foreach(RaycastHit h in hits){
-						if(g.Equals(h.collider.gameObject)){
-							//	Debug.Log(g.name+"found in racasts");
-							exists = true;
-							break;
-						}else{
-							exists = false;
-						}
-					}
-					//if it doesn't exist, re enable the renderer and add it the the list of items to be removed
-					if(!exists){
-						//Debug.Log(g.name+"removed");
-						//g.GetComponent<Renderer>().enabled = true;
-						if(g != null){
-							makeSolid(g.GetComponent<Renderer>());
-						}
-						toRemove.Add(g);
-					}
-				}
-				//remove then clear the list of items to be removed
-				foreach(GameObject g in toRemove){
-					fadeSet.Remove(g);
-				}
-				toRemove.Clear();
-			}
-		}else{
-			if(fadeSet != null){
-				if(fadeSet.Count > 0){
-					//if there is nothing being hit, we clear the fade set and re enable the renderers
-					foreach(GameObject g in fadeSet){
-						//g.GetComponent<Renderer>().enabled = true;
-						if(g != null){
-							makeSolid(g.GetComponent<Renderer>());
-						}
-						toRemove.Add(g);
-					}
-					//remove then clear the list of items to be removed
-					foreach(GameObject g in toRemove){
-						fadeSet.Remove(g);
-					}
-					toRemove.Clear();
-				}
-			}
-		}
-		}catch(UnityException e){
-			e.ToString();
-			fadeSet.Clear(); 
-		}
-	}
-	// Update is called once per frame
-	void FixedUpdate () {
-		fadeBlockingObjects();
-		fadeBackObjects();
-	}
+    void FadeTo(Renderer r, float targetAlpha, float speed)
+    {
+        foreach (var m in r.materials) // important: per-instance material
+        {
+            Color c = m.color;
+            c.a = Mathf.MoveTowards(c.a, targetAlpha, speed * Time.deltaTime);
+            m.color = c;
+        }
+    }
 }
